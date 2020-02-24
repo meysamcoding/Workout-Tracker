20f5a25c735da0719939708652d083ccbe88d7f5
Meysam NajafiTavani <meysamnajafi67@yahoo.com>
2020-02-24 10:35:19 -0800

    save file

---
+++ models/Workout.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                unique: true,
+                required: true,
+                default: ""
+            },
+
+            name: {
+                type: String,
+                required: true,
+                default: ""
+            },
+            duration: {
+                type: Number,
+                required: true,
+
+            },
+
+            weight: {
+                type: Number,
+                required: true
+            },
+
+            reps: {
+                type: Number,
+                required: true
+            },
+
+            sets: {
+                type: Number,
+                required: true
+            },
+
+            distance: {
+                type: Number,
+                required: true
+            },
+
+
+
+
+            longstring: {
+                type: String,
+                validate: [({ length }) => length >= 6, "Longstring should be longer."]
+            }
+
+
+        }
+    ]
+
+});
+
+const Exercise = mongoose.model("Example", ExerciseSchema);
+
+module.exports = Exercise;
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+const Workout = require("./models/Workout.js");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+                .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+app.put("/api/workouts/:id", (req, res)=>{
+
+    db.Workout.find(body)
+    .then(({_id}) => db.Workout.findByIdAndUpdate({}, { $push: { exercises:body } }, { new: true }))
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+})
+
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Exercise.find({})
+        .sort({ date: -1 })
+        .then(dbExercise => {
+            res.json(dbExercise);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
